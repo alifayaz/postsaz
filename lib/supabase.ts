8a3c903e72c26323526adf1768238e2353658702
a.fayaz <a.fayaz@safarbooking.com>
2025-06-12 14:28:41 +0330

    Add not found page

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "your-supabase-url"
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "your-supabase-anon-key"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://agjdeylhwrvsrouftljn.supabase.co"
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnamRleWxod3J2c3JvdWZ0bGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MTA1MzIsImV4cCI6MjA2NTI4NjUzMn0.yUUc5GM92v8POTeBmTcNeS5x76Akki3z1WTbXC5ZViQ"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
